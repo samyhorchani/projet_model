94c4747b1f3119b8abbb75150a80ff1dc983a618
samyhorchani <samyhorchani@gmail.com>
2023-12-19 17:25:32 +0100

    suppr

--- naive_mult_algo.c
+++
@@ -1,5 +0,0 @@
-/* Samy HORCHANI (n°étudiant : 28706765)*/
-
-#include "naive_mult_algo.h"
-#include <stdio.h>
-#include <stdlib.h>
--- naive_mult_algo.h
+++
@@ -1,8 +0,0 @@
-/* Samy HORCHANI (n° étudiant : 28706765) */
-
-#ifndef NAIVE_MULT_ALGO_H
-#define NAIVE_MULT_ALGO_H
-
-void naiveMultiplyPolynomials(int p1[], int size_p1, int p2[], int size_p2);
-
-#endif
